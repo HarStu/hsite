cc2a0a636692f77963e46a8796073c85d1a000da
Harrison <33430956+HarStu@users.noreply.github.com>
2025-07-02 16:39:02 -0400

    Copy over public files

--- build/build.ts
+++ build/build.ts
@@ -6,7 +6,10 @@ import MarkdownIt from 'markdown-it'
 const md = new MarkdownIt()
 
 const mdDir = path.resolve(__dirname, '../md')
-const files = await fs.readdir(mdDir)
+const mds = await fs.readdir(mdDir)
+
+const pubDir = path.resolve(__dirname, '../public')
+const pubs = await fs.readdir(pubDir)
 
 const templatePath = path.resolve(__dirname, '../templates/template.html')
 const template = await fs.readFile(templatePath, 'utf-8')
@@ -14,11 +17,14 @@ const template = await fs.readFile(templatePath, 'utf-8')
 const distDir = path.resolve(__dirname, '../dist/')
 await fs.mkdir(distDir, { recursive: true })
 
+const distPubDir = path.resolve(__dirname, '../dist/public')
+await fs.mkdir(distPubDir, { recursive: true })
+
 const srcCss = path.resolve(__dirname, '../templates/styles.css')
 const distCss = path.resolve(__dirname, '../dist/styles.css')
 await fs.copyFile(srcCss, distCss)
 
-for (const file of files) {
+for (const file of mds) {
   if (!file.endsWith('.md')) {
     console.log('non-markdown file')
     continue
@@ -41,6 +47,10 @@ for (const file of files) {
     slug,
     frontmatter: data,
   })
+}
 
-
+for (const file of pubs) {
+  const src = path.join(pubDir, file)
+  const dest = path.join(distPubDir, file)
+  await fs.copyFile(src, dest)
 }
