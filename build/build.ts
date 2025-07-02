c6ec6deb3e05e9ff9b9d4d971dd6bb5f44e441c1
Harrison <33430956+HarStu@users.noreply.github.com>
2025-07-02 16:12:39 -0400

    Static site gen

--- build/build.ts
+++ build/build.ts
@@ -8,6 +8,16 @@ const md = new MarkdownIt()
 const mdDir = path.resolve(__dirname, '../md')
 const files = await fs.readdir(mdDir)
 
+const templatePath = path.resolve(__dirname, '../templates/template.html')
+const template = await fs.readFile(templatePath, 'utf-8')
+
+const distDir = path.resolve(__dirname, '../dist/')
+await fs.mkdir(distDir, { recursive: true })
+
+const srcCss = path.resolve(__dirname, '../templates/styles.css')
+const distCss = path.resolve(__dirname, '../dist/styles.css')
+await fs.copyFile(srcCss, distCss)
+
 for (const file of files) {
   if (!file.endsWith('.md')) {
     console.log('non-markdown file')
@@ -19,10 +29,18 @@ for (const file of files) {
 
   const html = md.render(content)
 
+  const filledHtml = template
+    .replace('{{header}}', data.title ?? file.replace(/\.md$/, ''))
+    .replace('{{content}}', html)
+
+  const slug = file.replace(/\.md$/, '')
+  const outPath = path.join(distDir, `${slug}.html`)
+  await fs.writeFile(outPath, filledHtml, 'utf-8')
+
   console.log({
-    slug: file.replace(/\.md$/, ''),
+    slug,
     frontmatter: data,
-    html
   })
+
 
 }
